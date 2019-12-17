ae22b476cd15700df6597a3a1eaf3cfbd50bf5a4
mgryszko <marcin.gryszko@lastminute.com>
2019-12-17 07:38:05 +0100

    Exercise 1.3

---
+++ src/main/scala/sandbox/exercise13.scala
@@ -0,0 +1,35 @@
+package sandbox
+
+final case class Cat(name: String, age: Int, color: String)
+
+trait Printable[A] {
+  def format(value: A): String
+}
+
+object PrintableInstances {
+  implicit val stringPrintable: Printable[String] = (value: String) => value
+
+  implicit val intPrintable: Printable[Int] = (value: Int) => value.toString
+
+  implicit val catPrintable: Printable[Cat] = (value: Cat) =>
+    s"${Printable.format(value.name)} is a ${Printable.format(value.age)} year-old ${Printable.format(value.color)} cat."
+}
+
+object Printable {
+  def format[A](value: A)(implicit printable: Printable[A]): String =
+    printable.format(value)
+
+  def print[A](value: A)(implicit printable: Printable[A]) {
+    println(format(value))
+  }
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit printable: Printable[A]): String = Printable.format(value)
+
+    def print(implicit printable: Printable[A]) {
+      Printable.print(value)
+    }
+  }
+}
