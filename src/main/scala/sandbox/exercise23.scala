5030c54f22d65e548cef950fb84d9b60e33736b5
mgryszko <marcin.gryszko@lastminute.com>
2019-12-21 08:00:28 +0100

    Exercise 2.3

---
+++ src/main/scala/sandbox/exercise23.scala
@@ -0,0 +1,29 @@
+package sandbox
+
+object LogicalInstances {
+  import cats.Monoid
+
+  implicit val andMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    def empty: Boolean = true
+
+    def combine(x: Boolean, y: Boolean): Boolean = x && y
+  }
+
+  implicit val orMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    def empty: Boolean = false
+
+    def combine(x: Boolean, y: Boolean): Boolean = x || y
+  }
+
+  implicit val xorMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    def empty: Boolean = false
+
+    def combine(x: Boolean, y: Boolean): Boolean = (x && !y) || (!x && y)
+  }
+
+  implicit val xnorMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    def empty: Boolean = true
+
+    def combine(x: Boolean, y: Boolean): Boolean = (x || !y) && (!x || y)
+  }
+}
