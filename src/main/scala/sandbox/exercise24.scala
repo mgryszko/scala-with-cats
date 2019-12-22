93138a22e63cbd0c1832f55c51fd14f206aabe72
mgryszko <marcin.gryszko@lastminute.com>
2019-12-22 15:54:06 +0100

    Exercise 2.4

---
+++ src/main/scala/sandbox/exercise24.scala
@@ -0,0 +1,20 @@
+package sandbox
+
+object SetInstances {
+  import cats.Monoid
+  import cats.Semigroup
+
+  implicit def unionMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    def empty: Set[A] = Set.empty
+
+    def combine(x: Set[A], y: Set[A]): Set[A] = x ++ y
+  }
+
+  implicit def intersectionSemigroup[A]: Semigroup[Set[A]] = (x: Set[A], y: Set[A]) => x intersect  y
+
+  implicit def symmetricDifferenceMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    def empty: Set[A] = Set.empty
+
+    def combine(x: Set[A], y: Set[A]): Set[A] = (x -- y) ++ (y -- x)
+  }
+}
