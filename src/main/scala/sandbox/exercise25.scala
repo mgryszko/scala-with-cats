d27d65643409cae0a6c902f62f558c7cacd3b7d4
mgryszko <marcin.gryszko@lastminute.com>
2019-12-26 08:20:40 +0100

    Exercise 2.5 part 2 and 3

--- src/main/scala/sandbox/exercise25.scala
+++ src/main/scala/sandbox/exercise25.scala
@@ -1,5 +1,18 @@
 package sandbox
 
+import cats.Monoid
+
 object SuperAdder {
-  def add(items: List[Int]): Int = items.foldLeft(0)(_ + _)
+  case class Order(totalCost: Double, quantity: Double)
+
+  import cats.instances.double._
+  import cats.syntax.monoid._
+
+  implicit val orderMonoid: Monoid[Order] = new Monoid[Order] {
+    def empty: Order = Order(Monoid[Double].empty, Monoid[Double].empty)
+
+    def combine(x: Order, y: Order): Order = Order(x.totalCost |+| y.totalCost, x.quantity |+| y.quantity)
+  }
+
+  def add[A: Monoid](items: List[A]): A = items.foldLeft(Monoid[A].empty)(Monoid[A].combine)
 }
