62b7b848d0badddae25710e89369f5fd425f7e37
mgryszko <marcin.gryszko@lastminute.com>
2020-01-09 18:49:18 +0100

    Exercise 3.4

---
+++ src/main/scala/sandbox/exercise34.scala
@@ -0,0 +1,16 @@
+package sandbox
+
+import cats.Functor
+
+sealed trait Tree[+A]
+final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+final case class Leaf[A](value: A) extends Tree[A]
+
+object TreeInstances {
+  implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
+    def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+      case Leaf(value) => Leaf(f(value))
+    }
+  }
+}
