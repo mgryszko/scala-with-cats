d11aef48af977ee7bc12130f3d7972f795d34efd
mgryszko <marcin.gryszko@lastminute.com>
2020-01-14 07:45:23 +0100

    Exercise 4.3

--- src/main/scala/sandbox/exercise41.scala
+++ src/main/scala/sandbox/exercise41.scala
@@ -1,6 +1,6 @@
 package sandbox.chapter4
 
-trait Monad[F[_]] {
+trait MyMonad[F[_]] {
   def pure[A](a: A): F[A]
   def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
   def map[A, B](value: F[A])(func: A => B): F[B] =
---
+++ src/main/scala/sandbox/exercise43.scala
@@ -0,0 +1,10 @@
+package sandbox.chapter4
+
+import cats.{Id, Monad}
+
+class IdMonad extends Monad[Id] {
+  def pure[A](x: A): Id[A] = x
+  def flatMap[A, B](fa: Id[A])(f: A => Id[B]): B = f(fa)
+  def tailRecM[A, B](a: Id[A])(f: A => Id[Either[A, B]]): Id[B] = f(a).right.get
+}
+
