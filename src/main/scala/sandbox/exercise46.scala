f5aaddc6dc41b55b78ced519ed7975a49074bb13
mgryszko <marcin.gryszko@lastminute.com>
2020-01-16 07:15:21 +0100

    Exercise 4.6

---
+++ src/main/scala/sandbox/exercise46.scala
@@ -0,0 +1,24 @@
+package sandbox.chapter4
+
+import cats.Eval
+
+object UnsafeFold {
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    as match {
+      case head :: tail =>
+        fn(head, foldRight(tail, acc)(fn))
+      case Nil => acc
+    }
+}
+
+object SafeFold {
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    evalFoldRight(as, Eval.now(acc)) { (a, b) => b.map(fn(a, _))}.value
+
+  def evalFoldRight[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
+    as match {
+      case head :: tail =>
+        Eval.defer(fn(head, evalFoldRight(tail, acc)(fn)))
+      case Nil => acc
+    }
+}
