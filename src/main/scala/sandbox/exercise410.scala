501cd646e5381dbb2814b92a3599e9a10f35b14d
mgryszko <marcin.gryszko@lastminute.com>
2020-01-23 07:25:50 +0100

    Exercise 4.10

---
+++ src/main/scala/sandbox/exercise410.scala
@@ -0,0 +1,36 @@
+package sandbox.chapter4
+
+import cats.Monad
+
+sealed trait Tree[+A]
+
+final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+final case class Leaf[A](value: A) extends Tree[A]
+
+object Tree {
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+
+  def leaf[A](value: A): Tree[A] = Leaf(value)
+}
+
+object TreeInstances {
+  implicit def treeMonad[A]: Monad[Tree] = new TreeMonad[A]
+}
+
+class TreeMonad[A] extends Monad[Tree] {
+  def pure[A](x: A): Tree[A] = Leaf(x)
+
+  def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] =
+    fa match {
+      case Leaf(a) => f(a)
+      case Branch(l, r) => Branch(flatMap(l)(f), flatMap(r)(f))
+    }
+
+  def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] = {
+    flatMap(f(a)) {
+      case Left(value) => tailRecM(value)(f)
+      case Right(value) => Leaf(value)
+    }
+  }
+}
