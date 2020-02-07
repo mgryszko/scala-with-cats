cd36b470d453f268ab5027e30716e53c823876e4
mgryszko <marcin.gryszko@lastminute.com>
2020-02-07 18:38:40 +0100

    Exercise 6.3

---
+++ src/main/scala/sandbox/exercise63.scala
@@ -0,0 +1,12 @@
+package sandbox.chapter6
+
+import cats.Monad
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+
+object Product {
+  def product[M[_]: Monad, A, B](x: M[A], y: M[B]): M[(A, B)] = for {
+    a <- x
+    b <- y
+  } yield (a, b)
+}
