5cb54f1b56e85664199b56077493886e6bf0a4f7
mgryszko <marcin.gryszko@lastminute.com>
2020-02-12 07:02:16 +0100

    Exercise 7.2

---
+++ src/main/scala/sandbox/exercise72.scala
@@ -0,0 +1,29 @@
+package sandbox.chapter7
+
+import cats.Applicative
+import cats.data.Validated
+import cats.instances.list._
+import cats.instances.option._
+import cats.syntax.applicative._
+import cats.syntax.apply._
+
+object ListTraversal {
+  def listTraverse[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
+    list.foldLeft(List.empty[B].pure[F]) { (accum, item) =>
+      (accum, func(item)).mapN(_ :+ _)
+    }
+
+  def listSequence[F[_]: Applicative, B] (list: List[F[B]]): F[List[B]] =
+    listTraverse(list)(identity)
+
+  def processOption(inputs: List[Int]) =
+    listTraverse(inputs)(n => if(n % 2 == 0) Some(n) else None)
+
+  type ErrorsOr[A] = Validated[List[String], A]
+
+  def processVaidated(inputs: List[Int]): ErrorsOr[List[Int]] =
+    listTraverse(inputs) { n =>
+      if(n % 2 == 0) Validated.valid(n)
+      else Validated.invalid(List(s"$n is not even"))
+    }
+}
