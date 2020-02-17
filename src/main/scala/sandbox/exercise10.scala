342b650433b7e62a1c3bc525c740e3e8cf5c790f
mgryszko <marcin.gryszko@lastminute.com>
2020-02-17 22:57:11 +0100

    Exercise 10 - Validated

--- src/main/scala/sandbox/exercise10.scala
+++ src/main/scala/sandbox/exercise10.scala
@@ -1,24 +1,19 @@
 package sandbox
 
+import cats.data.Validated
 import cats.kernel.Semigroup
-import cats.syntax.semigroup._
-import cats.syntax.either._
+import cats.syntax.apply._
 
 sealed trait Check[E, A] {
-  def apply(value: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
+  def apply(value: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
     case Pure(func) => func(value)
-    case And(check1, check2) => (check1(value), check2(value)) match {
-      case (Right(_), Right(_)) => value.asRight
-      case (Right(_), Left(e)) => e.asLeft
-      case (Left(e), Right(_)) => e.asLeft
-      case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
-    }
+    case And(check1, check2) => (check1(value), check2(value)).mapN((_, _) => value)
   }
 
   def and(that: Check[E, A]): Check[E, A] = And(this, that)
 }
 
-final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
+final case class Pure[E, A](func: A => Validated[E, A]) extends Check[E, A]
 
 final case class And[E, A](check1: Check[E, A], check2: Check[E, A]) extends Check[E, A]
 
