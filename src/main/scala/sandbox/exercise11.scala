e4224d2beeedbe00f04db4a28ccac8b0a96f62a1
mgryszko <marcin.gryszko@lastminute.com>
2020-02-21 18:27:07 +0100

    Exercise 11 - GCounter as type class

--- src/main/scala/sandbox/exercise11.scala
+++ src/main/scala/sandbox/exercise11.scala
@@ -4,14 +4,27 @@ import cats.kernel.{BoundedSemilattice, CommutativeMonoid, Monoid}
 import cats.instances.map._
 import cats.syntax.monoid._
 
-final case class GCounter[K, V](counters: Map[K, V]) {
-  def increment(key: K, value: V)(implicit m: Monoid[V]): GCounter[K, V] =
-    GCounter(counters.updated(key, counters.getOrElse(key, m.empty) |+| value))
+trait GCounter[F[_,_],K, V] {
+  def increment(f: F[K, V])(k: K, v: V)(implicit m: CommutativeMonoid[V]): F[K, V]
 
-  def merge(that: GCounter[K, V])(implicit bs: BoundedSemiLattice[V]): GCounter[K, V] =
-    GCounter(counters |+| that.counters)
+  def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V]
 
-  def total(implicit m: CommutativeMonoid[V]): V = m.combineAll(counters.values)
+  def total(f: F[K, V])(implicit m: CommutativeMonoid[V]): V
+}
+
+object GCounter {
+  def apply[F[_,_], K, V](implicit counter: GCounter[F, K, V]) =
+    counter
+
+  implicit def mapInstance[K, V]: GCounter[Map, K, V] = new GCounter[Map, K, V] {
+    override def increment(f: Map[K, V])(key: K, value: V)(implicit m: CommutativeMonoid[V]): Map[K, V] =
+    f.updated(key, f.getOrElse(key, m.empty) |+| value)
+
+    override def merge(f1: Map[K, V], f2: Map[K, V])(implicit bs: BoundedSemiLattice[V]): Map[K, V] =
+    f1 |+| f2
+
+    override def total(f: Map[K, V])(implicit m: CommutativeMonoid[V]): V = m.combineAll(f.values)
+  }
 }
 
 trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
