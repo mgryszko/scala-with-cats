668986ca17fdbb7987df956b9f0fe5cbf4e2a998
mgryszko <marcin.gryszko@lastminute.com>
2020-02-21 06:57:48 +0100

    Exercise 11 - BoundedSemiLattice

--- src/main/scala/sandbox/exercise11.scala
+++ src/main/scala/sandbox/exercise11.scala
@@ -1,7 +1,9 @@
 package sandbox.exercise11
 
+import cats.kernel.{BoundedSemilattice, CommutativeMonoid}
+
 final case class GCounter(counters: Map[String, Int]) {
-  def increment(machine: String, amount: Int) =
+  def increment(machine: String, amount: Int): GCounter =
     GCounter(counters.updated(machine, counters.getOrElse(machine, 0) + amount))
 
   def merge(that: GCounter): GCounter =
@@ -11,3 +13,24 @@ final case class GCounter(counters: Map[String, Int]) {
 
   def total: Int = counters.values.sum
 }
+
+trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
+  def combine(a1: A, a2: A): A
+
+  def empty: A
+}
+
+object BoundedSemiLattice {
+  implicit def setInstance[A](): BoundedSemilattice[Set[A]] = new BoundedSemilattice[Set[A]] {
+    def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 union a2
+
+    def empty: Set[A] = Set.empty
+  }
+
+  implicit val intInstance: BoundedSemiLattice[Int] = new BoundedSemiLattice[Int] {
+    def combine(a1: Int, a2: Int): Int = a1 max a2
+
+    def empty: Int = 0
+  }
+}
+
